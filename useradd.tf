567de0d4b2cb37522dd865191895192b2c27aa34
Bala22041995 <akshaybhalkar12345@gmail.com>
2021-11-11 17:45:57 +0530

    commot

--- useradd.tf
+++ useradd.tf
@@ -1,4 +1,14 @@
-resource "aws_instance" "ec2-1" {
-     ami = "ami-0f19d220602031" 
-     instance_type = "t2.micro"
+provider "aws" {
+  region     = "us-east-2"
+  access_key = "AKIAQFBLMXI3QSEYSS3K"
+  secret_key = "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"
+
+}
+
+resource "aws_instance" "this" {
+  ami           = "ami-0f19d220602031aed"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "Web Server Test"
+  }
 }
